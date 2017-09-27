1a25aea2d773e41962cbec98e3d6700f1b920118
Lander Brandt <landerbrandt@gmail.com>
2017-09-26 23:48:05 -0700

    Include setting up sublime

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+SUBLIME_PATH="$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
+
+echo "Initializing submodules..."
+echo ""
 
 git submodule init
 git submodule update
+
+echo ""
+echo "Adding dotfiles"
 
 for file in `ls -l`; do
 	if [[ "$file" =~ ^\. ]]; then
@@ -9,3 +19,12 @@ for file in `ls -l`; do
 		ln -s "$PWD/$file" "$PWD/../$file"
 	fi
 done
+
+echo ""
+echo "Adding application-specific links..."
+
+rm -rf $SUBLIME_PATH
+ln -s "$PWD/Sublime/User" "$SUBLIME_PATH/User"
+
+echo ""
+echo "Done!"
