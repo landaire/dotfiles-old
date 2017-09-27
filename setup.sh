75aee41d1caf12d9e353a1e57d094a44f1a9de10
Lander Brandt <landerbrandt@gmail.com>
2017-09-26 23:56:29 -0700

    Add homebrew to setup

--- setup.sh
+++ setup.sh
@@ -3,12 +3,37 @@ set -euo pipefail
 IFS=$'\n\t'
 
 SUBLIME_PATH="$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
+BREW_PACKAGES="zsh ripgrep python3 fzf"
+BREW_CASK_PACKAGES="gpgtools"
 
 echo "Initializing submodules..."
 echo ""
 
 git submodule init
 git submodule update
+
+if ! [[ -x /usr/local/bin/brew ]]; then
+	echo ""
+	echo "Installing homebrew"
+
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+done
+
+if ! [[ -x /usr/local/bin/brew ]]; then
+	echo ""
+	echo "Installing homebrew"
+
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+done
+
+echo ""
+echo "Installing brew formulae..."
+
+brew install "$BREW_PACKAGES"
+
+echo ""
+echo "Installing brew casks..."
+brew cask install "$BREW_CASK_PACKAGES"
 
 echo ""
 echo "Adding dotfiles"
